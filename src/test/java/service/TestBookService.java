da91aa78a7d5ad44285987777e5e79e4afdcd172
oguzhantasgin@hotmail.com <Oguzhant93?>
2018-08-31 14:12:08 +0300

    Added test classes,code optimized

---
+++ src/test/java/service/TestBookService.java
@@ -0,0 +1,56 @@
+package service;
+
+
+import com.config.AppConfig;
+import com.config.WebAppInitializer;
+import com.config.WebConfig;
+import com.dao.BookDAO;
+import com.dao.PublisherDAO;
+import com.model.Book;
+import com.model.Publisher;
+import com.util.DateJSONValueProcessor;
+import net.sf.json.JSONArray;
+import net.sf.json.JsonConfig;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {AppConfig.class, WebConfig.class, WebAppInitializer.class})
+@WebAppConfiguration
+@Transactional
+public class TestBookService {
+
+
+    @Autowired
+    private BookDAO bookDAO;
+
+    @Autowired
+    private PublisherDAO publisherDAO;
+
+
+    @Test
+    public void testLoadBooksJson() {
+
+
+        List<Book> bookList = bookDAO.loadBooks();
+        JsonConfig jsonConfig = new JsonConfig();
+        DateJSONValueProcessor dateProc = new DateJSONValueProcessor("dd/MM/yyyy");
+        jsonConfig.registerJsonValueProcessor(Date.class, dateProc);
+        JSONArray jsonArray = JSONArray.fromObject(bookList, jsonConfig);
+
+        Assert.assertTrue(jsonArray.size() != 0);
+
+
+    }
+
+
+}
---
+++ src/test/java/service/TestPublisherService.java
@@ -0,0 +1,45 @@
+package service;
+
+
+import com.config.AppConfig;
+import com.config.WebAppInitializer;
+import com.config.WebConfig;
+import com.dao.PublisherDAO;
+import com.model.Publisher;
+import net.sf.json.JSONArray;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {AppConfig.class, WebConfig.class, WebAppInitializer.class})
+@WebAppConfiguration
+@Transactional
+public class TestPublisherService {
+
+    @Autowired
+    private PublisherDAO publisherDAO;
+
+
+
+    @Test
+    public void testLoadPublishersJson(){
+
+        List<Publisher> publisherList = publisherDAO.loadPublishers();
+        JSONArray jsonArray = JSONArray.fromObject(publisherList);
+
+        Assert.assertTrue(jsonArray.size() != 0);
+
+
+    }
+
+
+
+}
