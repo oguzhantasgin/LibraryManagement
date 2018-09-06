777da456d6379c2237647186a032718be47af8a6
oguzhantasgin@hotmail.com <Oguzhant93?>
2018-09-06 11:06:00 +0300

    Spring Security integrated. Created login, register, user management views.

--- src/main/java/com/config/AppConfig.java
+++ src/main/java/com/config/AppConfig.java
@@ -2,10 +2,15 @@
 package com.config;
 
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 import com.model.library.Book;
 import com.model.library.Publisher;
+import com.model.user.PersistentLogin;
+import com.model.user.User;
+import com.model.user.UserProfile;
+import com.model.user.UserProfileType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -22,10 +27,11 @@ import java.util.Properties;
 
 import static org.hibernate.cfg.AvailableSettings.*;
 
+@Configuration
 @PropertySource("classpath:hibernate.properties")
 @EnableTransactionManagement
-@Configuration
-@ComponentScan(basePackages = {"com"})
+
+@ComponentScan(basePackages = "com")
 public class AppConfig {
 
     @Autowired
@@ -34,9 +40,9 @@ public class AppConfig {
     @Bean
     public LocalSessionFactoryBean getSessionFactory() {
         LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
-       /* factoryBean.setDataSource(dataSource());*/
+        factoryBean.setDataSource(dataSource());
+        factoryBean.setAnnotatedClasses(Book.class, Publisher.class, PersistentLogin.class, User.class, UserProfile.class, UserProfileType.class);
         factoryBean.setHibernateProperties(putProperties(new Properties()));
-        factoryBean.setAnnotatedClasses(Book.class, Publisher.class);
         return factoryBean;
     }
 
@@ -47,42 +53,38 @@ public class AppConfig {
         return transactionManager;
     }
 
-/*
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(env.getRequiredProperty("jdbc.driverClassName"));
-        dataSource.setUrl(env.getRequiredProperty("jdbc.url"));
-        dataSource.setUsername(env.getRequiredProperty("jdbc.username"));
-        dataSource.setPassword(env.getRequiredProperty("jdbc.password"));
+        dataSource.setDriverClassName(env.getRequiredProperty("mysql.driver"));
+        dataSource.setUrl(env.getRequiredProperty("mysql.url"));
+        dataSource.setUsername(env.getRequiredProperty("mysql.username"));
+        dataSource.setPassword(env.getRequiredProperty("mysql.password"));
         return dataSource;
-    }*/
+    }
 
 
     @Bean
     public Properties putProperties(Properties properties) {
-        // Setting JDBC properties
-        properties.put(DRIVER, env.getProperty("mysql.driver"));
-        properties.put(URL, env.getProperty("mysql.url"));
-        properties.put(USER, env.getProperty("mysql.username"));
-        properties.put(PASS, env.getProperty("mysql.password"));
+
 
         // Setting Hibernate properties
-        properties.put(SHOW_SQL, env.getProperty("hibernate.show_sql"));
-        properties.put(FORMAT_SQL, env.getProperty("hibernate.format_sql"));
-        properties.put(HBM2DDL_AUTO, env.getProperty("hibernate.hbm2ddl.auto"));
-        properties.put(DIALECT, env.getProperty("hibernate.dialect"));
+        properties.put(SHOW_SQL, env.getRequiredProperty("hibernate.show_sql"));
+        properties.put(FORMAT_SQL, env.getRequiredProperty("hibernate.format_sql"));
+        properties.put(HBM2DDL_AUTO, env.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        properties.put(DIALECT, env.getRequiredProperty("hibernate.dialect"));
 
         // Setting C3P0 properties
-        properties.put(C3P0_MIN_SIZE, env.getProperty("hibernate.c3p0.min_size"));
-        properties.put(C3P0_MAX_SIZE, env.getProperty("hibernate.c3p0.max_size"));
-        properties.put(C3P0_ACQUIRE_INCREMENT, env.getProperty("hibernate.c3p0.acquire_increment"));
-        properties.put(C3P0_TIMEOUT, env.getProperty("hibernate.c3p0.timeout")); // seconds
-        properties.put(C3P0_MAX_STATEMENTS, env.getProperty("hibernate.c3p0.max_statements"));
-        properties.put(C3P0_CONFIG_PREFIX + ".initialPoolSize", env.getProperty("hibernate.c3p0.initialPoolSize"));
-        properties.put(C3P0_IDLE_TEST_PERIOD, env.getProperty("hibernate.c3p0.idle_test_period")); // seconds
-        properties.put(C3P0_CONFIG_PREFIX + ".acquireRetryAttempts", env.getProperty("hibernate.c3p0.acquireRetryAttempts"));
-        properties.put(C3P0_CONFIG_PREFIX + ".acquireRetryDelay", env.getProperty("hibernate.c3p0.acquireRetryDelay")); // milliseconds
+        properties.put(C3P0_MIN_SIZE, env.getRequiredProperty("hibernate.c3p0.min_size"));
+        properties.put(C3P0_MAX_SIZE, env.getRequiredProperty("hibernate.c3p0.max_size"));
+        properties.put(C3P0_ACQUIRE_INCREMENT, env.getRequiredProperty("hibernate.c3p0.acquire_increment"));
+        properties.put(C3P0_TIMEOUT, env.getRequiredProperty("hibernate.c3p0.timeout")); // seconds
+        properties.put(C3P0_MAX_STATEMENTS, env.getRequiredProperty("hibernate.c3p0.max_statements"));
+        properties.put(C3P0_CONFIG_PREFIX + ".initialPoolSize", env.getRequiredProperty("hibernate.c3p0.initialPoolSize"));
+        properties.put(C3P0_IDLE_TEST_PERIOD, env.getRequiredProperty("hibernate.c3p0.idle_test_period")); // seconds
+        properties.put(C3P0_CONFIG_PREFIX + ".acquireRetryAttempts", env.getRequiredProperty("hibernate.c3p0.acquireRetryAttempts"));
+        properties.put(C3P0_CONFIG_PREFIX + ".acquireRetryDelay", env.getRequiredProperty("hibernate.c3p0.acquireRetryDelay")); // milliseconds
 
 
         return properties;
